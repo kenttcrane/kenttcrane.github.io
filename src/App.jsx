239c83310ce9909c628fd969956c3e8b539deb40
kenttcrane <crane4869@yahoo.co.jp>
2025-05-11 21:23:39 +0900

    アカウント一覧をカードで表示できるように変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import {Box, Card, CardContent, CardActions, CardMedia, Typography, CardActionArea} from '@mui/material'
 
 function App() {
 
@@ -7,17 +8,32 @@ function App() {
       <div className='title'>kenttcraneのページ</div>
 
       <h2>アカウント一覧</h2>
-      <div className='accounts'>
-        <a href='https://github.com/kenttcrane'>
-          <img src='./images/github_logo.svg' className='logo' />
-        </a>
-        <a href='https://mstdn.jp/@kenttcrane'>
-          <img src='./images/mastodon_logo.svg' className='logo' />
-        </a>
-        <a href='https://note.com/kenttcrane'>
-          <img src='images/note_logo.svg' className='logo' />
-        </a>
-      </div>
+      <Box display='flex' justifyContent='center' gap={2}>
+        <Card className='card'>
+          <CardActionArea component='a' href='https://github.com/kenttcrane' className='card' sx={{ display: 'flex', flexDirection: 'column' }}>
+            <CardMedia component='img' src='./images/github_logo.svg' className='logo' />
+            <CardContent sx={{ marginTop: 'auto' }}>
+                <Typography variant='h6'>&gt;&gt; Github</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card className='card'>
+          <CardActionArea component='a' href='https://mstdn.jp/@kenttcrane' className='card' sx={{ display: 'flex', flexDirection: 'column' }}>
+            <CardMedia component='img' src='./images/mastodon_logo.svg' className='logo' />
+            <CardContent sx={{ marginTop: 'auto' }}>
+                <Typography variant='h6'>&gt;&gt; Mastodon</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+        <Card className='card'>
+          <CardActionArea component='a' href='https://note.com/kenttcrane' className='card' sx={{ display: 'flex', flexDirection: 'column' }}>
+            <CardMedia component='img' src='./images/note_logo.svg' className='logo' />
+            <CardContent sx={{ marginTop: 'auto' }}>
+                <Typography variant='h6'>&gt;&gt; note</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Box>
     </>
   )
 }
