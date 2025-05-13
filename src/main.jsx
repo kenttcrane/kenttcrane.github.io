fe294f574b74739611be180b059655eb7121cb5c
kenttcrane <crane4869@yahoo.co.jp>
2025-05-13 19:55:51 +0900

    Revert "修正 - react-router-domがgithub pagesで機能しない問題を修正"
    This reverts commit 949bd33c0521cc0dc553455e41e8c15916022b61.

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {AppRoutes} from './Routes.jsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <AppRoutes />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
